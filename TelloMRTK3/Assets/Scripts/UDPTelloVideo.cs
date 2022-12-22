2a775f3afbf7f6d3f31824f9ff08744c6cb31632
ZaidZaim <zaidzaim2000@gmail.com>
2022-12-22 22:39:28 +0100

    Refactoring TelloVideo UDP Script

--- TelloMRTK3/Assets/Scripts/UDPTelloVideo.cs
+++ TelloMRTK3/Assets/Scripts/UDPTelloVideo.cs
@@ -1,87 +1,72 @@
 using System;
+using System.Collections;
 using System.Net;
 using System.Net.Sockets;
 using UnityEngine;
-using System.Threading.Tasks;
-using System.Collections;
+using UnityEngine.UI;
 
 public class UDPTelloVideo : MonoBehaviour
 {
     private const float INTERVAL = 0.2f;
-    public event Action<string> OnReceive;
 
     private string localIp = "";
     private int localPort = 11111;
     private UdpClient udpClient;
     private IPEndPoint telloAddress;
 
+    // Add a RawImage component to display the video
+    public RawImage videoImage;
+
     private void Start()
     {
-        
         udpClient = new UdpClient(localPort);
+
         string telloIp = "192.168.10.1";
         int telloPort = 8889;
         telloAddress = new IPEndPoint(IPAddress.Parse(telloIp), telloPort);
         udpClient.Send(System.Text.Encoding.UTF8.GetBytes("command"), "command".Length, telloAddress);
-        //Debug.Log("sleep");
-        //StartCoroutine(Sleep(2));
-        //udpClient.Send(System.Text.Encoding.UTF8.GetBytes("streamon"), "streamon".Length, telloAddress);
-        StartListening();
+        StartCoroutine(Delay(2.0f));
+        udpClient.Send(System.Text.Encoding.UTF8.GetBytes("streamon"), "streamon".Length, telloAddress);
+        
+        IPEndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+        byte[] response = udpClient.Receive(ref remote);
 
+        // Create a new Texture2D object and set its size based on the size of the video frame
+        Texture2D videoTexture = new Texture2D(176, 144);
+        videoTexture.LoadImage(response);
+
+        // Set the texture of the RawImage component to the video frame
+        videoImage.texture = videoTexture;
+        Debug.Log(videoImage);
+
+
+        // Start the co-routine to receive and process the video frames
+        // StartCoroutine(ReceiveVideoFrames());
     }
 
-
-    private void StartListening()
+    private IEnumerator ReceiveVideoFrames()
     {
-        var task = Task.Run(() =>
+        while (true)
         {
-            while (true)
-            {
-                IPEndPoint remote = new IPEndPoint(IPAddress.Any, 0);
-                byte[] response = udpClient.Receive(ref remote);
-                string responseString = System.Text.Encoding.UTF8.GetString(response);
-                OnReceive?.Invoke(responseString);
-                Debug.Log("This is the message you received " +
-                                responseString.ToString());
-                Debug.Log("This message was sent from " +
-                                            remote.Address.ToString() +
-                                            " on their port number " +
-                                            remote.Port.ToString());
-                Debug.Log(responseString);
+            IPEndPoint remote = new IPEndPoint(IPAddress.Any, 0);
+            byte[] response = udpClient.Receive(ref remote);
 
+            // Create a new Texture2D object and set its size based on the size of the video frame
+            Texture2D videoTexture = new Texture2D(176, 144);
+            videoTexture.LoadImage(response);
 
-                if (responseString == "ok")
-                {
-                    return;
-                }
+            // Set the texture of the RawImage component to the video frame
+            videoImage.texture = videoTexture;
 
-                string outString = responseString.Replace(";", ";\n");
-                outString = "Tello Stream data:\n" + outString;
-                Debug.Log(outString);
-            }
-        });
+            // Destroy the videoTexture object to avoid memory leaks
+            Destroy(videoTexture);
+
+            yield return new WaitForSeconds(INTERVAL);
+        }
     }
 
-
-    IEnumerator Sleep(int seconds)
+    IEnumerator Delay(float seconds)
     {
         yield return new WaitForSeconds(seconds);
     }
 }
-
-/**private void Update()
-{
-    IPEndPoint remote = new IPEndPoint(IPAddress.Any, 0);
-    byte[] response = udpClient.Receive(ref remote);
-    string responseString = System.Text.Encoding.UTF8.GetString(response);
-
-    if (responseString == "ok")
-    {
-        return;
-    }
-
-    string outString = responseString.Replace(";", ";\n");
-    outString = "Tello State:\n" + outString;
-    Debug.Log(outString);
-    Invoke("Update", INTERVAL);
-}**/
