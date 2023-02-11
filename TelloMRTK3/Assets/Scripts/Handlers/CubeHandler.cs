0ab5e7769a6d899ef1fbab7058476db88d447dd5
ZaidZaim <zaidzaim2000@gmail.com>
2023-02-11 18:49:08 +0100

    Cube as interaction possibility
    Movments of the cube initiate the behavior of the drone (distance, intial & final  cube postion, movement is initiated when cube is released)

--- TelloMRTK3/Assets/Scripts/Handlers/CubeHandler.cs
+++ TelloMRTK3/Assets/Scripts/Handlers/CubeHandler.cs
@@ -4,17 +4,70 @@ using UnityEngine;
 
 public class CubeHandler : MonoBehaviour
 {
-    public GameObject cube;
-
+    [SerializeField] private GameObject cube;
+    [SerializeField] private DroneHandler droneHandler;
+    private Vector3 initialPostion;
+    private Vector3 finalPosition;
+   
     private void Update()
     {
-        Vector3 objectPosition= cube.transform.position;
-        Rigidbody objectphysics =GetComponent<Rigidbody>();
-
+        Vector3 objectPosition = cube.transform.position;
         
-        Debug.Log(objectPosition);
-        Debug.Log(objectphysics.velocity);
+
+
+
+        //Debug.Log(objectPosition);
+        //Debug.Log(objectphysics.velocity);
 
     }
+    public void IsGrabbed() 
+    {
+        initialPostion = cube.transform.position;
+        Debug.Log("Cube is grabbed");
+
+    }
+    public void IsReleased()
+    {
+        finalPosition = cube.transform.position;
+        Vector3 distance = Distance(initialPostion, finalPosition);
+        InitiateMovement(distance);
+
+        Debug.Log("Cube is released");
+    }
+
+    private void InitiateMovement(Vector3 direction)
+    {
+        if (direction.x<0)
+        {
+            droneHandler.MoveLeft(Mathf.Abs(direction.x) * 100);
+            
+            // Move left
+        }
+        else if (direction.x >0)
+        {
+            droneHandler.MoveRight(Mathf.Abs(direction.x)*100);
+            // Move Right
+        }
+        if (direction.y<0)
+        {
+            // Move Down
+        }
+
+        else if (direction.y>0)
+        {
+            // Move up
+
+        } 
+
+
+    }
+
+    private static Vector3 Distance(Vector3 initialPostion, Vector3 finalPosition) 
+    {
+        Vector3 distiance = finalPosition - initialPostion;
+
+        return distiance;
+    
+    } 
 
 }
